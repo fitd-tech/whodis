8c92efa728a4c25eb16ce11198b330da9e27165c
Tony Cook <tony@pipesearch.com>
2025-07-15 13:56:08 -0500

    Update footer colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import randomColor from 'randomcolor'
+import convert from 'color-convert'
 
 import './App.css'
 
@@ -16,8 +17,26 @@ function App() {
       setButtonLabelClassName('new-tone-slide-out')
 
       const backgroundColor = randomColor()
+      const [backgroundHslHue, backgroundHslSaturation, backgroundHslLightness] = convert.hex.hsl(backgroundColor)
+      let textHslLightness = 50
+      let linkHslLightness = 50
+      if (backgroundHslLightness <= 50) {
+        textHslLightness = 70
+        linkHslLightness = 80
+      } else {
+        textHslLightness = 30
+        linkHslLightness = 20
+      }
+      const contrastingTextColor = '#' + convert.hsl.hex([backgroundHslHue, backgroundHslSaturation, textHslLightness])
+      const contrastingLinkColor = '#' + convert.hsl.hex([backgroundHslHue, backgroundHslSaturation, linkHslLightness])
+
       const root = document.getElementsByTagName('html')[0]
+      const [byline] = document.getElementsByClassName('byline') as HTMLCollectionOf<HTMLElement>
+      const [bylineLink1, bylineLink2] = document.getElementsByClassName('byline-link') as HTMLCollectionOf<HTMLElement>
       root.style.setProperty('background-color', backgroundColor)
+      byline.style.setProperty('color', contrastingTextColor)
+      bylineLink1.style.setProperty('color', contrastingLinkColor)
+      bylineLink2.style.setProperty('color', contrastingLinkColor)
     }
   }
 
@@ -75,10 +94,10 @@ function App() {
       </div>
       <div className="byline">
         <div className="byline-text">
-          A collection of fun concepts <span className="no-line-break">by <a href="mailto:tony@fitd.tech">Anthony Peluso-Cook</a>.</span>
+          A collection of fun concepts <span className="no-line-break">by <a href="mailto:tony@fitd.tech" className="byline-link">Anthony Peluso-Cook</a>.</span>
         </div>
         <div className="fitd-link">
-          <a href ="https://fitd.tech">See my other projects</a>.
+          <a href ="https://fitd.tech" className="byline-link">See my other projects</a>.
         </div>
       </div>
     </>
